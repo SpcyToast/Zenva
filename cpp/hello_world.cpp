60e4d13648a0783ea3b13db8a7ed30a238520957
Prashniel Kumar <prashniel@gmail.com>
2024-04-08 17:04:44 +1200

    Hello World in C++

---
+++ cpp/hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //Standard input/output streams library
+
+//the main function acts as an entry point into a program
+int main() {
+    //printing to the console
+    std::cout << "Hello World!\n";
+}
+
+// g++ -o [Filename] hello_world.cpp to complile
+// ./[Filename] to run
