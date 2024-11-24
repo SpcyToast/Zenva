9dc88c2f35eabb74532d17cecba889b70a17bea8
Prashniel Kumar <prashniel@gmail.com>
2024-11-24 17:19:36 +1300

    const

---
+++ cpp/variables/constants.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+
+  // constants are denoted with const and are variables that cannot be changed
+  const std::string email = "john_smith@email.com";
+  std::cout << "email is" << email << std::endl;
+
+}
