0b10066bcd3e63c76ed192118acb3209c6534bf7
Prashniel Kumar <prashniel@gmail.com>
2024-12-07 23:37:40 +1300

    increment and decrement modification

--- cpp/operators/increment.cpp
+++ cpp/operators/increment.cpp
@@ -7,4 +7,8 @@ int main(){
   std::cout << "next year age is " << age << std::endl;
   age--;
   std::cout << "This year age is " << age << std::endl;
+  int addAge;
+  std::cout << "Input number to find out how old you'll be after all those years" << std::endl;
+  std::cin >> addAge;
+  std::cout << "You will be " <<  (age += addAge) << " after " << addAge << " years" << std::endl;
 }
