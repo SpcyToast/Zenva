e2d772bfb457882beec5222ac472eb999e90e421
Prashniel Kumar <prashniel@gmail.com>
2024-12-07 23:27:42 +1300

    increment and decrement

---
+++ cpp/operators/increment.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+  int age = 24;
+  std::cout << "current age is " << age << std::endl;
+  age++;
+  std::cout << "next year age is " << age << std::endl;
+  age--;
+  std::cout << "This year age is " << age << std::endl;
+}
